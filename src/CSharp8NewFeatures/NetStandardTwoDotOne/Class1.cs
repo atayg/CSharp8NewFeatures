c03b99b78e9dfbb0caa93b629d42dcf0c3e1c9a5
Gökhan Atay <gokhan.atay@d-teknoloji.com.tr>
2022-05-30 22:11:35 +0300

    I deleted standard projects folders.

--- src/CSharp8NewFeatures/NetStandardTwoDotOne/Class1.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace NetStandardTwoDotOne
-{
-    public class Class1
-    {
-
-    }
-}
--- src/CSharp8NewFeatures/NetStandardTwoDotZero/Class1.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace NetStandardTwoDotZero
-{
-    public class Class1
-    {
-
-    }
-}
